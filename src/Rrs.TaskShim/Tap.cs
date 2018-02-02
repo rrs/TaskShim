843fa90a8f60cbf841f6a69f0b7df98dc7f4ee93
unknown <bobblezor@gmail.com>
2018-02-02 18:22:18 +0000

    added from result and CompletedTask shim

--- src/Rrs.TaskShim/Tap.cs
+++ src/Rrs.TaskShim/Tap.cs
@@ -73,6 +73,22 @@ namespace Rrs.TaskShim
             return tcs.Task;
         }
 
+        public static Task<T> FromResult<T>(T value)
+        {
+            var tcs = new TaskCompletionSource<T>();
+            tcs.SetResult(value);
+            return tcs.Task;
+        }
+
+        public static Task CompletedTask => CompletedTaskImpl();
+
+        private static Task CompletedTaskImpl()
+        {
+            var tcs = new TaskCompletionSource<object>();
+            tcs.SetResult(null);
+            return tcs.Task;
+        }
+
 
         public static Task IterateTasks(IEnumerable<Task> asyncIterator)
         {
